0d4dc54c2792ae9b7c1f75d6634929aba8385800
Lorry Benoit <lbenoit@microstrat.com>
2019-11-01 10:36:59 -0400

    initial commit

---
+++ src/test/java/com/lorry/spring/sample/spring5demo/Spring5demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorry.spring.sample.spring5demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
